23c10165f4939fc7def8287f25bba5c92ad422b0
Akash <akash@Digio-Akash-Kumar.local>
2023-11-20 18:36:39 +0530

    readme add

---
+++ DigioUpi.xcframework/ios-arm64_x86_64-simulator/DigioUpi.framework/Headers/DigioUpi.h
@@ -0,0 +1,18 @@
+//
+//  DigioUpi.h
+//  DigioUpi
+//
+//  Created by Akash  on 30/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DigioUpi.
+FOUNDATION_EXPORT double DigioUpiVersionNumber;
+
+//! Project version string for DigioUpi.
+FOUNDATION_EXPORT const unsigned char DigioUpiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DigioUpi/PublicHeader.h>
+
+
